821f64802b1bb68c66bfd56221348e4554946851
Ramon Chiara <ramonchiara@gmail.com>
2018-06-16 02:11:28 -0300

    desafio2 em 82.48

--- src/main/python/desafio2.py
+++ src/main/python/desafio2.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import requests
 
-df_train = pd.read_csv('train.csv').fillna(0)
+df_train = pd.read_csv('train.csv').dropna()
 
 df_train['NOTA_FINAL'] = (3.0 * df_train['NU_NOTA_MT'] +
                           2.0 * df_train['NU_NOTA_CN'] +
@@ -15,11 +15,25 @@ df_train['NOTA_FINAL'] = (3.0 * df_train['NU_NOTA_MT'] +
 
 # criar modelo
 
+from sklearn.linear_model import LinearRegression
+
+x = df_train['NOTA_FINAL'].values.reshape(-1, 1)
+y = df_train['NU_NOTA_MT'].values.reshape(-1, 1)
+
+model = LinearRegression(fit_intercept=True)
+model.fit(x, y)
+
 # usar modelo para predição
 
-df = pd.read_csv('test2.csv')
+df = pd.read_csv('test2.csv').fillna(0)
 
-# df['NU_NOTA_MT'] = usar modelo para predição
+df['NOTA_FINAL'] = (3.0 * df_train['NU_NOTA_MT'].mean() +
+                    2.0 * df['NU_NOTA_CN'] +
+                    1.5 * df['NU_NOTA_LC'] +
+                    1.0 * df['NU_NOTA_CH'] +
+                    3.0 * df['NU_NOTA_REDACAO']) / (3.0 + 2.0 + 1.5 + 1.0 + 3.0)
+
+df['NU_NOTA_MT'] = model.predict(df['NOTA_FINAL'].values.reshape(-1, 1))
 
 df_answer = df[['NU_INSCRICAO', 'NU_NOTA_MT']]
 
