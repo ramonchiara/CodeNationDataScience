59b9e8ac14fa003856c46a7b5b11ad9cd2c6355b
Ramon Chiara <ramonchiara@gmail.com>
2018-06-17 20:42:23 -0300

    desafio2 em 89.04

--- src/main/python/desafio2.py
+++ src/main/python/desafio2.py
@@ -5,35 +5,20 @@ import requests
 
 df_train = pd.read_csv('train.csv').fillna(0)
 
-df_train['NOTA_FINAL'] = (3.0 * df_train['NU_NOTA_MT'] +
-                          2.0 * df_train['NU_NOTA_CN'] +
-                          1.5 * df_train['NU_NOTA_LC'] +
-                          1.0 * df_train['NU_NOTA_CH'] +
-                          3.0 * df_train['NU_NOTA_REDACAO']) / (3.0 + 2.0 + 1.5 + 1.0 + 3.0)
+import statsmodels.api as sm
 
-# procurar melhor correlacionamento
+x = df_train[['NU_NOTA_CN', 'NU_NOTA_LC', 'NU_NOTA_CH', 'NU_NOTA_REDACAO']]
+y = df_train['NU_NOTA_MT']
 
-# criar modelo
-
-from sklearn.linear_model import LinearRegression
-
-x = df_train['NOTA_FINAL'].values.reshape(-1, 1)
-y = df_train['NU_NOTA_MT'].values.reshape(-1, 1)
-
-model = LinearRegression(fit_intercept=True)
-model.fit(x, y)
-
-# usar modelo para predição
+model = sm.OLS(y, x).fit()
 
 df = pd.read_csv('test2.csv').fillna(0)
 
-df['NOTA_FINAL'] = (3.0 * df_train['NU_NOTA_MT'].mean() +
-                    2.0 * df['NU_NOTA_CN'] +
-                    1.5 * df['NU_NOTA_LC'] +
-                    1.0 * df['NU_NOTA_CH'] +
-                    3.0 * df['NU_NOTA_REDACAO']) / (3.0 + 2.0 + 1.5 + 1.0 + 3.0)
+x = df[['NU_NOTA_CN', 'NU_NOTA_LC', 'NU_NOTA_CH', 'NU_NOTA_REDACAO']]
 
-df['NU_NOTA_MT'] = model.predict(df['NOTA_FINAL'].values.reshape(-1, 1))
+predictions = model.predict(x)
+
+df['NU_NOTA_MT'] = predictions.values.reshape(-1, 1)
 
 df_answer = df[['NU_INSCRICAO', 'NU_NOTA_MT']]
 
@@ -45,5 +30,5 @@ envio = {
     "answer": answer
 }
 
-r = requests.post('https://api.codenation.com.br/v1/user/acceleration/data-science/challenge/enem-2/submit', json = envio)
+r = requests.post('https://api.codenation.com.br/v1/user/acceleration/data-science/challenge/enem-2/submit', json=envio)
 print(r.text)
